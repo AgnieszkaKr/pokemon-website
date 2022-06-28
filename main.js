d242c6c0a630eabe2c166913f508769476d702ce
Agnes <agsawicka@gmail.com>
2022-06-28 11:24:09 -0400

    commit

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 // alert("JavaScript is working")
-let pokemon = ["Charmander", "Squirtle", "Blastoise"];
-let pokemonIds = ["001", "002", "003", "004", "005", "006"];
+// let pokemon = ["bulbasaur", "charmander", "squirtle"];
+let pokemonIds = ["001", "002", "003"];
+const pokemon = [
+    {name: "Bulbasaur", id: "001"},
+    {name: "Bharmander", id: "002"},
+    {name: "Squirtle", id: "003"}
+]
 let containerDiv = document.querySelector('#container')
 
-pokemonIds.map((id) =>{
-    let imgUrl =`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
-    console.log('img url', imgUrl)
-    let div =document.createElement('div')
-    div.setAttribute("class", "pokemon-card")
-    let img = document.createElement('img')
+pokemon.map((element, index) =>{
+    console.log(element);
+    let imgUrl =`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
+    let div = document.createElement('div');
+    let h3 =document.createElement("h1");
+    h3.innerText = element.name;
+    div.setAttribute("class", "pokemon-card");
+    let img = document.createElement('img');
     img.src = imgUrl;
     // img.setAttribute('height', '100px')
     // img.setAttribute('width', '100px')
-    div.append(img)
-    document.body.append(div)
+    div.append(img, h3);
+    containerDiv.append(div);
 })
+
+// sprite -pixeled picture
 
