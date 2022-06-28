1405a1c0c00dadf66462def344743d806b4aeaea
Agnes <agsawicka@gmail.com>
2022-06-28 15:01:47 -0400

    adding new card

--- main.js
+++ main.js
@@ -6,13 +6,39 @@ const pokemon = [
     {name: "Charmander", id: "002"},
     {name: "Squirtle", id: "003"}
 ]
-let containerDiv = document.querySelector('#container')
-const newBtn = document.querySelector('#new-pokemon-btn')
 
-newBtn.addEventListener('click', () =>{
+const userRoster = [];
+
+const containerDiv = document.querySelector('#container')
+const newBtn = document.querySelector('#new-pokemon-btn')
+const rosterDiv = document.querySelector('#roster');
+
+newBtn.addEventListener('click', async () =>{
     let num = prompt("Enter a Pokemon number:")
     console.log("Number entered:", num)
-})
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${num}.png`
+    let dataUrl = `https://pokeapi.co/api/v2/pokemon/${num}`
+    let req = await fetch(dataUrl)
+    let res = await req.json()
+    let name = res.forms[0].name;
+    let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${name.toLowerCase()}.mp3`
+    let audio = document.createElement("audio");
+    let source = document.createElement("source");
+    source.setAttribute("src", audioUrl);
+    source.setAttribute("type", "audio/mpeg");
+    audio.append(source);
+    let h3 = document.createElement("h3");
+    h3.innerText = name 
+    let img = document.createElement('img');
+    img.setAttribute('src', imgUrl)
+    img.setAttribute('class', 'roster-img')
+    let position = document.querySelector(`#pokemon-${userRoster.length+1}`);
+    position.addEventListener('click', () =>{
+        audio.play()
+    })
+    position.append(img, h3, audio);
+    userRoster.push(num);
+    })
 
 pokemon.map((element, index) =>{
     console.log(element);
@@ -35,7 +61,7 @@ pokemon.map((element, index) =>{
         audio.play()
     })
     img.src = imgUrl;
-    const rosterDiv = document.getElementById("roster");
+    
     // setTimeout(() => {
         
     // }, 5000);
